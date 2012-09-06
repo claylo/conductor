76ee726eb85ee721fd7beb6e0317833e9122f64d
Clay Loveless <clay@jexy.com>
2012-09-06 00:31:41 -0700

    experimenting with finding root install

--- Conductor/Tasks/BootstrapSymfonyStandard.php
+++ Conductor/Tasks/BootstrapSymfonyStandard.php
@@ -13,13 +13,22 @@ class BootstrapSymfonyStandard
 {
     public static function postInstall(Event $event)
     {
-        // $package = $event->getComposer()->getPackage();
-        // $install_path = $event
-        //                     ->getComposer()
-        //                     ->getInstallationManager()
-        //                     ->getInstallPath($package);
+        $package = $event->getComposer()->getPackage();
+        $vendor_dir = $event->getComposer()->getConfig()->get('vendor-dir');
+        $install_path = $event
+                            ->getComposer()
+                            ->getInstallationManager()
+                            ->getInstallPath($package);
 
+        $install_path = str_replace(
+            DIRECTORY_SEPARATOR . $vendor_dir . DIRECTORY_SEPARATOR . $package->getName(),
+            '',
+            $install_path
+        );
+        echo "install path v1: $install_path\n";
+        
+        // Works, but hacky
         $install_path = realpath(__DIR__ . '/../../../../../');
-        echo "install path: $install_path\n";
+        echo "install path v2: $install_path\n";
     }
 }
